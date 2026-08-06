20b5430359f527848d6084f9ed422859e2ae2fcb
shooukie <bigcape@naver.com>
2026-08-06 16:11:09 +0900

    feat: 페이지 라우트 path 상수 추가

---
+++ src/routes/paths.ts
@@ -0,0 +1,24 @@
+export const PATH = {
+  LANDING: "/",
+  AUTH: "/auth",
+
+  // HomePage
+  HOME: "/home",
+
+  //MissionPage
+  MISSIONS: "/missions",
+  MISSION_DETAIL: "/missions/:missionId",
+
+  //MapPage
+  MAP: "/map",
+
+  //RewardsPage
+  REWARDS: "/rewards",
+
+  // MyPage
+  MY: "/my",
+  QUESTY_CUSTOMIZE: "/my/customize",
+
+  // Error
+  NOT_FOUND: "*",
+} as const;
